5853d093e9c3460b88a16e8420e889de97b603e1
RobertoCampos3030 <camposcarlos693@gmail.com>
2022-10-10 20:27:41 -0500

    nuevo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { ReservasComponent } from './reservas/reservas.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { MascotasComponent } from './mascotas/mascotas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientesComponent,
+    ReservasComponent,
+    ServiciosComponent,
+    MascotasComponent
   ],
   imports: [
     BrowserModule,
