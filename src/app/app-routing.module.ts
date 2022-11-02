20133787eb3e4a49cc62d2ca4aa2c8d507a5886a
RobertoCampos3030 <camposcarlos693@gmail.com>
2022-11-01 20:34:52 -0500

    12

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,45 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { MascotasComponent } from './pages/mascotas/mascotas.component';
+import { ReservasComponent } from './pages/reservas/reservas.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
 
-const routes: Routes = [];
+
+const routes: Routes=[
+  {
+      path:'clientes',
+      component:ClientesComponent,
+      
+  },
+  {
+      path:'mascotas',
+      component:MascotasComponent
+
+  },
+  {
+      path:'reserva',
+      component:ReservasComponent
+  },
+  {
+      path:'servicios',
+      component:ServiciosComponent
+  },
+  {
+      path:'**',
+      redirectTo :'' 
+  }
+
+
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports:[
+      RouterModule.forRoot( routes )
+  ],
+  exports:[
+      RouterModule
+  ]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule{}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ClientesComponent } from './clientes/clientes.component';
-import { ReservasComponent } from './reservas/reservas.component';
-import { ServiciosComponent } from './servicios/servicios.component';
-import { MascotasComponent } from './mascotas/mascotas.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClientesComponent,
-    ReservasComponent,
-    ServiciosComponent,
-    MascotasComponent
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
